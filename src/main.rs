7d1fe26f10aa19cd890e51e55b45337e591d730e
Simon Voordouw <simonvoordouw@gmail.com>
2021-06-27 11:43:06 +0200

    implement gravity, material densities and phases

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,54 @@
 use bevy::prelude::*;
 use bevy::reflect::TypeUuid;
 use bevy::render::camera::Camera;
+use bevy::utils::HashMap;
 use ndarray::{prelude::*, Zip};
 use std::ops::{Deref, DerefMut};
 
 #[derive(Debug, TypeUuid, Clone)]
 #[uuid = "3e6c203c-76a0-4acc-a812-8d48ee685e61"]
-struct Board(pub Array2<Cell>);
+struct Board(pub Array2<Material>);
 
 impl Board {
     pub fn new(width: usize, height: usize) -> Board {
-        Board(Array2::from_elem((width, height), Cell::Air))
+        Board(Array2::from_elem((width, height), Material::Air))
     }
 }
 
 impl Deref for Board {
-    type Target = Array2<Cell>;
-    fn deref(&self) -> &Array2<Cell> {
+    type Target = Array2<Material>;
+    fn deref(&self) -> &Array2<Material> {
         &self.0
     }
 }
 
 impl DerefMut for Board {
-    fn deref_mut(&mut self) -> &mut Array2<Cell> {
+    fn deref_mut(&mut self) -> &mut Array2<Material> {
         &mut self.0
     }
 }
 
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum Cell {
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum Material {
     Bedrock,
     Air,
     Sand,
+    Water,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum Phase {
+    Solid,
+    Liquid,
+    Gas,
 }
 
 struct ToolState {
-    draw_type: Cell
+    draw_type: Material,
 }
+
+struct MaterialDensities(HashMap<Material, u32>);
+struct MaterialPhases(HashMap<Material, Phase>);
 
 pub struct FallingSand {
     cells: Board,
@@ -64,19 +75,34 @@ impl FallingSand {
         }
     }
 
-    fn iterate(&mut self) {
-        self.scratch.view_mut().fill(Cell::Air);
+    fn gravity(
+        &mut self,
+        material_densities: &MaterialDensities,
+        material_phases: &MaterialPhases,
+    ) {
+        // TODO move this function to gravity_system, without tripping up the borrow checker
+        self.scratch.view_mut().fill(Material::Air);
 
         Zip::from(self.cells.windows((3, 3))).map_assign_into(
             &mut self.scratch.0,
             |neigh| unsafe {
-                if *neigh.uget((1, 1)) == Cell::Air && *neigh.uget((1, 0)) == Cell::Sand
-                    || *neigh.uget((1, 1)) == Cell::Sand && *neigh.uget((1, 2)) == Cell::Sand
-                    || *neigh.uget((1, 1)) == Cell::Sand && *neigh.uget((1, 2)) == Cell::Bedrock
-                {
-                    Cell::Sand
+                if neigh.iter().all(|material| *material == Material::Air) {
+                    return Material::Air;
+                }
+
+                let current_material = *neigh.uget((1, 1));
+                if current_material == Material::Bedrock {
+                    return current_material;
+                }
+
+                let material_below = *neigh.uget((1, 2));
+                let material_above = *neigh.uget((1, 0));
+                if material_phases.0.get(&material_below).unwrap() != &Phase::Solid && material_densities.0.get(&current_material).unwrap() > material_densities.0.get(&material_below).unwrap() {
+                    return material_below;
+                } else if material_phases.0.get(&material_above).unwrap() != &Phase::Solid && material_densities.0.get(&current_material).unwrap() < material_densities.0.get(&material_above).unwrap() {
+                    return material_above;
                 } else {
-                    Cell::Air
+                    return current_material;
                 }
             },
         );
@@ -89,9 +115,9 @@ fn main() {
     let mut app = App::build();
 
     app.insert_resource(WindowDescriptor {
-            mode: bevy::window::WindowMode::BorderlessFullscreen,
-            ..Default::default()
-        });
+        mode: bevy::window::WindowMode::BorderlessFullscreen,
+        ..Default::default()
+    });
 
     #[cfg(not(target_arch = "wasm32"))]
     app.add_plugins(DefaultPlugins);
@@ -100,10 +126,30 @@ fn main() {
     app.add_plugins(bevy_webgl2::DefaultPlugins);
 
     app.add_startup_system(setup.system())
+        .add_system(gravity_system.system())
         .add_system(grid_system.system())
         .add_system(draw_tool_system.system())
+        .add_system(switch_tool_system.system())
+        .insert_resource({
+            let mut map = HashMap::default();
+            map.insert(Material::Air, 0);
+            map.insert(Material::Water, 1);
+            map.insert(Material::Sand, 2);
+            map.insert(Material::Bedrock, 3);
+            MaterialDensities(map)
+        })
+        .insert_resource({
+            let mut map = HashMap::default();
+            map.insert(Material::Air, Phase::Gas);
+            map.insert(Material::Water, Phase::Liquid);
+            map.insert(Material::Sand, Phase::Liquid);
+            map.insert(Material::Bedrock, Phase::Solid);
+            MaterialPhases(map)
+        })
         .insert_resource(ClearColor(Color::WHITE))
-        .insert_resource(ToolState { draw_type: Cell::Sand })
+        .insert_resource(ToolState {
+            draw_type: Material::Sand,
+        })
         .run();
 }
 
@@ -117,8 +163,8 @@ fn setup(
 
     let a = {
         let mut a = Board::new(100, 100);
-        a.slice_mut(s![10..20, 1]).fill(Cell::Sand);
-        a.slice_mut(s![0..99, 99]).fill(Cell::Bedrock);
+        a.slice_mut(s![10..20, 1]).fill(Material::Sand);
+        a.slice_mut(s![0..99, 99]).fill(Material::Bedrock);
         a
     };
 
@@ -149,22 +195,40 @@ fn setup(
         .insert(FallingSand::new_from_board(&a, texture));
 }
 
-fn grid_system(mut grid_query: Query<&mut FallingSand>, mut textures: ResMut<Assets<Texture>>) {
+fn gravity_system(
+    mut grid_query: Query<&mut FallingSand>,
+    material_densities: Res<MaterialDensities>,
+    material_phases: Res<MaterialPhases>,
+) {
     for mut grid in grid_query.iter_mut() {
-        grid.iterate();
+        grid.gravity(&material_densities, &material_phases);
+    }
+}
 
+fn grid_system(grid_query: Query<&FallingSand>, mut textures: ResMut<Assets<Texture>>) {
+    for grid in grid_query.iter() {
         if let Some(texture) = textures.get_mut(&grid.texture) {
             texture.data = grid
                 .cells
                 .t()
                 .iter()
                 .flat_map(|cell| match *cell {
-                    Cell::Sand => [244, 215, 21, 255u8],
-                    Cell::Bedrock => [77, 77, 77, 255u8],
+                    Material::Sand => [244, 215, 21, 255u8],
+                    Material::Water => [255, 0, 0, 255u8],
+                    Material::Bedrock => [77, 77, 77, 255u8],
                     _ => [255u8, 255u8, 255u8, 255u8],
                 })
                 .collect();
         }
+    }
+}
+
+fn switch_tool_system(mut tool_state: ResMut<ToolState>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::Key1) {
+        tool_state.draw_type = Material::Sand;
+    }
+    if keyboard_input.pressed(KeyCode::Key2) {
+        tool_state.draw_type = Material::Water;
     }
 }
 
@@ -173,7 +237,7 @@ fn draw_tool_system(
     mut grid_query: Query<(&mut FallingSand, &GlobalTransform)>,
     mouse_button_input: Res<Input<MouseButton>>,
     camera_transforms: Query<&GlobalTransform, With<Camera>>,
-    tool_state: Res<ToolState>
+    tool_state: Res<ToolState>,
 ) {
     let maybe_window: Option<Vec3> = windows.get_primary().and_then(|window| {
         window.cursor_position().map(|cursor_position| {
@@ -204,8 +268,11 @@ fn draw_tool_system(
                 8,
             );
             if tile_position.0 > 0 && tile_position.1 > 0 {
-                if let Some(cell) = grid.cells.get_mut((tile_position.0 as usize, tile_position.1 as usize)) {
-                    if *cell != Cell::Bedrock {
+                if let Some(cell) = grid
+                    .cells
+                    .get_mut((tile_position.0 as usize, tile_position.1 as usize))
+                {
+                    if *cell != Material::Bedrock {
                         *cell = tool_state.draw_type;
                     }
                 }
